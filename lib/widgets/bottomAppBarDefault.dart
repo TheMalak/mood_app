406e6c9c312746a047cf612377fa8877c5e7b818
Malak <david.tejeda.santos@gmail.com>
2022-11-16 19:35:11 -0600

    initMoodApp

---
+++ lib/widgets/bottomAppBarDefault.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MoodBottomBar extends StatelessWidget {
+  const MoodBottomBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
